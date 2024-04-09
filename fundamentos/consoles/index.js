5d4bc65d2d983a0003b1a4573425812908ad01d6
David Henrique <dh210605@gmail.com>
2024-04-09 19:32:05 -0300

    recursão

---
+++ fundamentos/consoles/index.js
@@ -0,0 +1,22 @@
+const x = 10
+const y = 'David H.'
+const z = [1, 2]
+
+console.log(x, y, z)
+
+console.count(`o valor de X é ->  ${x}, contagem: `)
+console.count(`o valor de X é ->  ${x}, contagem: `)
+console.count(`o valor de X é ->  ${x}, contagem: `)
+console.count(`o valor de X é ->  ${x}, contagem: `)
+console.count(`o valor de X é ->  ${x}, contagem: `)
+console.count(`o valor de X é ->  ${x}, contagem: `)
+
+
+console.log('O nome dele é $s, ele é programador', y)
+
+setTimeout(() => {
+
+    console.clear()
+
+}, 2000
+)
